113cf2741d2d4237126f6eb38cf27ab4f3ac263c
Maxim <maxtrubitsyn3@mail.ru>
2025-10-02 17:00:36 +0300

    Научился прокидывать пропсы в компоненты

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,19 @@ import AppNews from "./components/AppNews.vue";
 export default {
   data() {
     return {
-      now: new Date().toLocaleDateString()
+      now: new Date().toLocaleDateString(),
+      news: [
+        { id: 0, title: "Какая-то новость 1" },
+        { id: 1, title: "Новая новость 2" },
+        { id: 2, title: "Ну точно новая новость 3" },
+        { id: 3, title: "Самая новая новость!! 4" },
+      ],
     };
   },
   components: {
     // AppNews,
     // AppNews: AppNews,
-    "app-news": AppNews
+    "app-news": AppNews,
   },
 };
 </script>
@@ -17,12 +23,12 @@ export default {
 <template>
   <div class="container">
     <h1 class="title">Новости на {{ now }}</h1>
-    <app-news></app-news>
-    <app-news></app-news>
-    <app-news></app-news>
-    <app-news></app-news>
-    <app-news></app-news>
-    <app-news></app-news>
+    <app-news
+      v-for="items in news"
+      :key="items.id"
+      :title="items.title"
+    ></app-news>
+    <!-- title="Статичные данные" так мы бы передали пропс со статикой -->
     <!-- <AppNews /> Если у нас camelCase, то тогда нужно использовать такой тег -->
   </div>
 </template>
