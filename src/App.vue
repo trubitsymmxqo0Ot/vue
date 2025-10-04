becfda5c9b635650487fef67dd0e173238c24054
Maxim <maxtrubitsyn3@mail.ru>
2025-10-04 20:43:23 +0300

    Реализовал небольшой практический пример
    *Теперь при нажатии на кнопку открыть она меняется на закрыть
    *Теперь можно пометить новость как прочитанной и наоборот, у этого ведется свой отдельный счетчик
    *Реализовал несколько emit под эти действия

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,22 @@ export default {
     return {
       now: new Date().toLocaleDateString(),
       openRate: 0,
+      counterRead: 0,
       news: [
-        { id: 0, title: "Какая-то новость 1", isOpen: false },
-        { id: 1, title: "Новая новость 2", isOpen: false },
-        { id: 2, title: "Ну точно новая новость 3", isOpen: false },
-        { id: 3, title: "Самая новая новость!! 4", isOpen: false },
+        { id: 0, title: "Какая-то новость 1", isOpen: false, wasRead: false },
+        { id: 1, title: "Новая новость 2", isOpen: false, wasRead: false },
+        {
+          id: 2,
+          title: "Ну точно новая новость 3",
+          isOpen: false,
+          wasRead: false,
+        },
+        {
+          id: 3,
+          title: "Самая новая новость!! 4",
+          isOpen: false,
+          wasRead: false,
+        },
       ],
     };
   },
@@ -19,25 +30,39 @@ export default {
     "app-news": AppNews,
   },
   methods: {
-    openNews(data, data2){
+    openNews(data, data2) {
       this.openRate++;
       console.log(data, data2);
+    },
+    readNews(id) {
+      const idx = this.news.findIndex(news => news.id === id);
+      this.news[idx].wasRead = true;
+      this.counterRead++;
+    },
+    unreadNews(id){
+      const idx = this.news.findIndex(news => news.id === id);
+      this.news[idx].wasRead = false;
+      this.counterRead--;
     }
-  }
+  },
 };
 </script>
 
 <template>
   <div class="container">
     <h1 class="title">Новости на {{ now }}</h1>
-    <span>Новость была открыта {{ openRate }} раз</span>
+    <span class="title__text">Новость была открыта {{ openRate }} раз</span>
+    <span class="title__text">Прочитано новостей {{ counterRead }} </span>
     <app-news
       v-for="items in news"
       :key="items.id"
       :title="items.title"
       :id="items.id"
       :is-open="items.isOpen"
+      :was-read="items.wasRead"
       @open-flag="openNews"
+      @mark-news="readNews"
+      @unmark-news="unreadNews"
     ></app-news>
     <!-- title="Статичные данные" так мы бы передали пропс со статикой -->
     <!-- <AppNews /> Если у нас camelCase, то тогда нужно использовать такой тег -->
--- src/components/AppNews.vue
+++ src/components/AppNews.vue
@@ -1,49 +1,70 @@
 <template>
-	<div class="news">
-		<p class="news__text">{{ title }}</p>
-		<button @click="open" class="news__button">Открыть</button>
-		<p v-if="isNewOpen">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis sunt natus quis eum itaque, veniam ullam vel corporis nihil. Ullam iste qui ab beatae voluptatibus? Voluptatibus quia necessitatibus aut eius?</p>
+  <div class="news">
+    <p class="news__text">{{ title }}</p>
+    <button @click="open" class="news__button">
+      {{ isNewOpen ? "Закрыть" : "Открыть" }}
+    </button>
+	<div v-if="isNewOpen">
+		<button class="news__button-reverse" v-if="wasRead" @click="unmark">Пометить новость непрочитанной</button>
+		<p>
+		  Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis sunt
+		  natus quis eum itaque, veniam ullam vel corporis nihil. Ullam iste qui ab
+		  beatae voluptatibus? Voluptatibus quia necessitatibus aut eius?
+		</p>
+		<hr />
+		<button class="news__button-reverse" @click="mark" v-if="!wasRead">Прочитать новость</button>
 	</div>
+  </div>
 </template>
 
 <script>
 export default {
-	props: {
-		title: String, //Валидация в одну строку, не тонкая валидация
-		id: {
-			type: Number,
-			required: true,
-		},
-		isOpen: {
-			type: Boolean,
-			required: false,
-			default: false,
-			validator(value){
-				return value === true || value === false
-			}
-		}
-	},	
-	// emits: ['open-flag'], можем указывать так, если типизация не нужна
-	emits: {
-		'open-flag'(num) {
-			if(num) {
-				return true
-			}
-			console.warn('Не получили 42')
-		}
-	},
-	data(){
-		return {
-			isNewOpen: this.isOpen,
-		}
-	},
-	methods: {
-		open() {
-			this.isNewOpen = !this.isNewOpen;
-			if(this.isNewOpen) {
-				this.$emit('open-flag', 42);
-			}
-		}
+  props: {
+    title: String, //Валидация в одну строку, не тонкая валидация
+    id: {
+      type: Number,
+      required: true,
+    },
+    isOpen: {
+      type: Boolean,
+      required: false,
+      default: false,
+      validator(value) {
+        return value === true || value === false;
+      },
+    },
+	wasRead: Boolean,
+  },
+  // emits: ['open-flag'], можем указывать так, если типизация не нужна
+  emits: {
+    "open-flag"(num) {
+      if (num) {
+        return true;
+      }
+      console.warn("Не получили 42");
+    },
+	"mark-news": null,
+	"unmark-news": null,
+  },
+  data() {
+    return {
+      isNewOpen: this.isOpen,
+    };
+  },
+  methods: {
+    open() {
+      this.isNewOpen = !this.isNewOpen;
+      if (this.isNewOpen) {
+        this.$emit("open-flag", 42);
+      }
+    },
+    mark() {
+      this.$emit("mark-news", this.id);
+    },
+	unmark(){
+		this.isNewOpen = false;
+		this.$emit('unmark-news', this.id);
 	}
-}
+  },
+};
 </script>
